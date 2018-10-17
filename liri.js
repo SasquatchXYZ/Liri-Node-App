3d4e4af31787a45c2468db1e8f0bcd10582a6173
SasquatchXYZ <dalton.ricker.2011@graduate.usmma.edu>
2018-10-17 17:00:25 -0400

    reformatted code for easier reading and fixed functionality of random.txt

--- liri.js
+++ liri.js
@@ -17,6 +17,84 @@ for (k = 3; k < process.argv.length; k++) {
 console.log(command, parameter);
 
 if (command === `concert-this`) {
+    queryBandsInTown();
+
+/*    let artist = parameter.join("%20");
+    let queryURL = `https://rest.bandsintown.com/artists/${artist}/events?app_id=codingbootcamp`;
+    console.log(artist);
+    console.log(queryURL);
+
+    request(queryURL, function(error, response, data) {
+        if (!error && response.statusCode === 200) {
+            let results = JSON.parse(data);
+            //console.log(results);
+            //console.log(results.length);
+            for (var k = 0; k < results.length; k++) {
+                console.log(`=======================================`);
+                console.log(`Venue Name: ${results[k].venue.name}`);
+                console.log(`Venue Location: ${results[k].venue.city}, ${results[k].venue.country}`);
+                console.log(`Date: ${moment(results[k].datetime).format("MM/DD/YYYY")}`);
+            }
+        }
+    });*/
+
+} else if (command === `spotify-this-song`) {
+    querySpotify();
+    /*spotify.search({type: `track`, query: `${parameter}`, limit: 1}, function(error, data) {
+        if (error) {
+            return console.log(`Error Occurred: ${error}`);
+        }
+        let result = data.tracks.items;
+        /!*console.log(result);*!/
+        let disc = result[0];
+        console.log(`Artist: ${disc.artists[0].name}`); //Band Name
+        console.log(`Song Title: ${disc.name}`); //Song Name
+        console.log(`Preview Link: ${disc.preview_url}`); //Preview URL
+        console.log(`Album Title: ${disc.album.name}`); //Album Title
+    });*/
+
+} else if (command === `movie-this`) {
+    queryOMDB();
+/*    let movieTitle = parameter.join("+");
+    if (movieTitle.length === 0) {
+        movieTitle = "Mr.+Nobody";
+    }
+    //console.log(movieTitle);
+
+    let queryURL = `http://www.omdbapi.com/?t=${movieTitle}&y=&plot=short&apikey=trilogy`;
+
+    request(queryURL, function(error, response, body) {
+        if (!error && response.statusCode === 200) {
+            //console.log(JSON.parse(body));
+            console.log(`Title: ${JSON.parse(body).Title}`);
+            console.log(`Year Released: ${JSON.parse(body).Year}`);
+            console.log(`IMDB Rating: ${JSON.parse(body).Ratings[0].Value}`);
+            console.log(`Rotten Tomatoes Rating; ${JSON.parse(body).Ratings[1].Value}`);
+            console.log(`Country: ${JSON.parse(body).Country}`);
+            console.log(`Language: ${JSON.parse(body).Language}`);
+            console.log(`Plot: ${JSON.parse(body).Plot}`);
+            console.log(`Actors: ${JSON.parse(body).Actors}`);
+        }
+    });*/
+
+} else if (command === `do-what-it-says`) {
+    queryRandom();
+/*    fs.readFile("random.txt", "utf8", function(error, data) {
+        if (error) {
+            return console.log(error);
+        }
+        //console.log(data);
+        var randomArray = data.split(",");
+        console.log(randomArray);
+        command = randomArray[0];
+        console.log(command);
+        parameter = randomArray[1];
+        console.log(parameter);
+    })*/
+}
+
+// Functions ===========================================================================================================
+function queryBandsInTown() {
     let artist = parameter.join("%20");
     let queryURL = `https://rest.bandsintown.com/artists/${artist}/events?app_id=codingbootcamp`;
     console.log(artist);
@@ -35,9 +113,12 @@ if (command === `concert-this`) {
             }
         }
     });
-
-} else if (command === `spotify-this-song`) {
-    spotify.search({type: `track`, query: `${parameter}`, limit: 1}, function(error, data) {
+}
+// =====================================================================================================================
+function querySpotify() {
+    let songTitle = parameter.join(" ");
+    //console.log(songTitle);
+    spotify.search({type: `track`, query: `${songTitle}`, limit: 1}, function(error, data) {
         if (error) {
             return console.log(`Error Occurred: ${error}`);
         }
@@ -49,9 +130,11 @@ if (command === `concert-this`) {
         console.log(`Preview Link: ${disc.preview_url}`); //Preview URL
         console.log(`Album Title: ${disc.album.name}`); //Album Title
     });
-
-} else if (command === `movie-this`) {
+}
+// =====================================================================================================================
+function queryOMDB() {
     let movieTitle = parameter.join("+");
+
     if (movieTitle.length === 0) {
         movieTitle = "Mr.+Nobody";
     }
@@ -72,19 +155,20 @@ if (command === `concert-this`) {
             console.log(`Actors: ${JSON.parse(body).Actors}`);
         }
     });
-
-} else if (command === `do-what-it-says`) {
+}
+// =====================================================================================================================
+function queryRandom() {
     fs.readFile("random.txt", "utf8", function(error, data) {
         if (error) {
             return console.log(error);
         }
-        //console.log(data);
+        console.log(data);
         var randomArray = data.split(",");
-        console.log(randomArray);
+        //console.log(randomArray);
         command = randomArray[0];
-        console.log(command);
-        parameter = randomArray[1];
-        console.log(parameter);
+        parameter = randomArray[1].replace(/"/g, "").split(" ");
+        console.log(command, parameter);
+
     })
 }
 

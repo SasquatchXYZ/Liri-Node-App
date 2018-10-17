3368ee7c122fadb0a4b51fed173e95b56347f407
SasquatchXYZ <dalton.ricker.2011@graduate.usmma.edu>
2018-10-17 01:15:23 -0400

    added spotify, moment, request, .dotenv

--- liri.js
+++ liri.js
@@ -1,3 +1,16 @@
 require("dotenv").config();
 
+
+let fs = require("fs");
+let request = require("request");
+let moment = require("moment");
+let keys = require("./keys");
+let Spotify = require("node-spotify-api");
+
 let spotify = new Spotify(keys.spotify);
+console.log(spotify);
+
+const [, , command, parameter] = process.argv;
+console.log(command, parameter);
+
+

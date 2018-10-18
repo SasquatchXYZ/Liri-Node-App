9ffb2e27c6b7b7f7bf3d9063d3f218099a9de76f
SasquatchXYZ <dalton.ricker.2011@graduate.usmma.edu>
2018-10-17 20:59:44 -0400

    working log.txt code just needs reformatting

--- liri.js
+++ liri.js
@@ -110,15 +110,28 @@ function queryOMDB() {
 
     request(queryURL, function(error, response, body) {
         if (!error && response.statusCode === 200) {
+            let moviedata = {};
+            moviedata.title = JSON.parse(body).Title;
+            moviedata.releaseYear = JSON.parse(body).Year;
+            moviedata.imdbRating = JSON.parse(body).Ratings[0].Value;
+            moviedata.rottenTomatoesRating = JSON.parse(body).Ratings[1].Value;
+            moviedata.country = JSON.parse(body).Country;
+            moviedata.language = JSON.parse(body).Language;
+            moviedata.plot = JSON.parse(body).Plot;
+            moviedata.actors = JSON.parse(body).Actors;
+
+            console.log(moviedata);
+
+            appendLog(JSON.stringify(moviedata));
             //console.log(JSON.parse(body));
-            console.log(`Title: ${JSON.parse(body).Title}`);
+            /*console.log(`Title: ${JSON.parse(body).Title}`);
             console.log(`Year Released: ${JSON.parse(body).Year}`);
             console.log(`IMDB Rating: ${JSON.parse(body).Ratings[0].Value}`);
             console.log(`Rotten Tomatoes Rating; ${JSON.parse(body).Ratings[1].Value}`);
             console.log(`Country: ${JSON.parse(body).Country}`);
             console.log(`Language: ${JSON.parse(body).Language}`);
             console.log(`Plot: ${JSON.parse(body).Plot}`);
-            console.log(`Actors: ${JSON.parse(body).Actors}`);
+            console.log(`Actors: ${JSON.parse(body).Actors}`);*/
         }
     });
 }
